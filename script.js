5af9ea68f105daec581d567a4dc70cf1f844e6d9
AndreiManache <manacheandrei@gmail.com>
2020-02-07 11:54:15 +0200

    upd

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
 
 
 
-
 function hide(){
     document.getElementById('note_elev_wrapper').classList.add("hide");
     document.querySelector('body').style.gridTemplateColumns="auto";
 }
 
-function show(){
+function show(str){
+
     document.getElementById('note_elev_wrapper').classList.remove("hide");
     document.querySelector('body').style.gridTemplateColumns="50% auto";
+    document.querySelector("#note_elev_wrapper h1").innerText = str;
+
+
+}
+
+class Elev{
+    constructor(nume){
+        this.nume=nume;
+        this.note=[];
+    }
+}
+
+class Catalog{
+    constructor(){
+        this.elevi=[];
+
+    }
+
+    addElev(str){
+        var elev = new Elev(str);
+        this.elevi.push(elev);
+    }
+
+    veziNote(){
+
+    }
+}
+var catalog = new Catalog();
+
+
+function onClickAddElev(e){
+e.preventDefault()
+
+    var elev = document.querySelector("#numeElev").value;
+    catalog.addElev(elev);
+    drawElevi()
+}
+
+function onClickVeziNote(){
+
+}
+
+
+function drawElevi(){
+    var str = "";
+
+    for(let i=0; i<catalog.elevi.length ; i++){
+        var elev = catalog.elevi[i];
+        str+=`
+        <tr>
+            <td>${elev.nume}</td>
+            <td></td>
+            <td><button class="btnNote" onclick="show(${elev.nume})">Detalii</button></td>
+        </tr>
+        `
+    }
+    document.querySelector("#tabel tbody").innerHTML = str;
 }
